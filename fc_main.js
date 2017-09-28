84196865ae76c18f39ff3306c03e6d77f8a0ca0b
Johannes-Kutsch <Johannes-Kutsch@users.noreply.github.com>
2017-09-28 20:40:51 +0200

    .

--- fc_main.js
+++ fc_main.js
@@ -1721,7 +1721,6 @@ function autoGSBuy() {
 
 function autoGodzamokAction() {
     if (Game.hasGod('ruin') && Game.Objects['Cursor'].amount > 10 && hasClickBuff()) {
-        Game.storeBulkButton(4);
         Game.Objects['Cursor'].sell(Game.Objects['Cursor'].amount);
     }
 }
@@ -1961,6 +1960,7 @@ function FCStart() {
     }
 
     if (FrozenCookies.autoGodzamok) {
+		Game.storeBulkButton(4);
         FrozenCookies.autoGodzamokBot = setInterval(autoGodzamokAction, FrozenCookies.frequency)
     }
 
