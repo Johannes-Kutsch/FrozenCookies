bbff120284693271d5b9e2a2df278eed80513cf5
Johannes-Kutsch <Johannes-Kutsch@users.noreply.github.com>
2017-09-28 20:20:26 +0200

    Revert "Removed faulty bulk purchase settings"
    This reverts commit 4466314601179e5bfd5aa5d2dd04cbe0fac67a90.

--- fc_main.js
+++ fc_main.js
@@ -1731,6 +1731,7 @@ function autoGodzamokAction() {
         Game.Objects['Shipment'].sell(Game.Objects['Shipment'].amount);
         Game.Objects['Alchemy lab'].sell(Game.Objects['Alchemy lab'].amount);
         Game.Objects['Portal'].sell(Game.Objects['Portal'].amount);
+        Game.storeBulkButton(2);
     }
 }
 
